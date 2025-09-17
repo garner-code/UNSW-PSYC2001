794992ffd4b61035f5d96cb30320f47e21123e9f
kel-github <getkellygarner@gmail.com>
2025-09-17 18:07:38 +1000

    redraft of chapter 1

---
+++ PSYC2001_ComputingTutorials/1_Introduction_to_R/your-first-script.R
@@ -0,0 +1,14 @@
+###############################################################################
+# Welcome to your first script.
+# This script will help you get started with R programming.
+# Follow the comments and complete the tasks.
+# K. Garner, 2025
+# To complete the exercises in this script, follow Chapter X from
+# insert book here
+###############################################################################
+
+# Paste 2 + 2 below and run it so you can see the output in the console
+
+
+# run the below line of code to see what answer you get
+mean(c(5, 3, 2))
