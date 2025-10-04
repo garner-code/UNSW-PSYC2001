ad836c9e29408d0c94ae428041c58153383ad95a
kelly <getkellygarner@gmail.com>
2025-10-04 14:21:40 +1000

    tut 3 completed

--- PSYC2001_ComputingTutorials/3_Testing-our-first-hypothesis/README.txt
+++ PSYC2001_ComputingTutorials/3_Testing-our-first-hypothesis/README.txt
@@ -1,14 +1,13 @@
-Data Wrangling and Visualisation
+The dataset we are using is called 'PSYC2001_social-media-data-cleaned.csv',
+The dataset is the same as 'PSYC2001_social-media-data.csv', except that -999s
+(missing values) have been replaced with NAs.
 
-Please read me before starting the tutorial. 
+and it can be found in the folder Data/
 
-This week you will be learning about importing CSV data into R, checking and cleaning data and visualising it. 
-
-Before beginning please read the brief summary of the dataset we will be using today. This dataset is called 'PSYC2001_social-media-data.csv'.
-
-Note – the key below is important, and you will need to refer to it throughout weeks 1–10.
-
-This dataset was collected as part of an experiment investigating social media use in young Australian adults. Sixty young adults answered questions about their **social media usage** as well as their **political attitudes**. Data about their **social media usage** (e.g., likes) was collected while they used their preferred platforms under various conditions.
+This dataset was collected as part of an experiment investigating social media use in young Australian adults. 
+Sixty young adults answered questions about their **social media usage** as well as their **political attitudes**. 
+Data about their **social media usage** (e.g., likes) was collected while they used their preferred platforms 
+under various conditions.
 
 The variables in the data are:
 
@@ -26,6 +25,6 @@ The next 3 columns are political attitude subscales:
 - campaign – how much they engage in campaign-related discussion
 - activism – involvement in activism (e.g., protests, petitions)
 
-You are now ready to start the tutorial!
+
 
 
--- PSYC2001_ComputingTutorials/3_Testing-our-first-hypothesis/testing-first-hypothesis.R
+++ PSYC2001_ComputingTutorials/3_Testing-our-first-hypothesis/testing-first-hypothesis.R
@@ -17,15 +17,15 @@ rm(list=ls()) # it is good practice to clear your environment at the start of
 if(!require(here)) install.packages('here') #checks if a package is installed and installs it if required.
 if(!require(tidyverse)) install.packages('tidyverse')
 
-library() #loads in the specified package
-library()
+library(here) #loads in the specified package
+library(tidyverse)
 
 
 ###############################################################################
 ## Loading and checking data
 
 # Now Amend the code below to read in your fresh data
-social_media <- read.csv(file = here("Data","PSYC2001_social-media-data-cleaned.csv"))
+social_media <- read.csv(file = here(???,"PSYC2001_social-media-data-cleaned.csv"))
 
 # now use the summary function to check your data, especially to make sure the pesky
 # -999 values are gone!
@@ -34,10 +34,55 @@ social_media <- read.csv(file = here("Data","PSYC2001_social-media-data-cleaned.
 ###############################################################################
 ## Get the data into longform format
 
-social_media_likes <- social_media %>% 
+## run this code so you can see what it does
+social_media %>% 
+  select("id","good_mood_likes","bad_mood_likes") # choose which columns we want keep in our dataframe
+
+## now run this code to pivot the data into longform format and see the results
+social_media %>% 
   select("id","good_mood_likes","bad_mood_likes") %>% # choose which columns we want keep in our dataframe
   pivot_longer(cols = ends_with("likes"), names_to = "mood", values_to = "likes")
+
+## now, very important! amend the code above so that the new dataframe is saved to an object called
+# social_media_likes. Then run the code to create the new dataframe.
 
 # check the new dataframe using the head function. Remember to make your code match what's in the book!
 (social_media_likes)
 
+
+###############################################################################
+## Plotting the data real pretty
+social_media_likes %>% 
+  ggplot(aes(x = likes, group = mood, fill = mood)) + # set the aesthetics that will be worked 
+  # with on the canvas
+  geom_density() # use the data to draw a density plot 
+
+
+###############################################################################
+## Get descriptive statistics
+
+social_media_likes %>% 
+  group_by(mood) %>% #split the data by mood
+  summarise(mean = mean(likes),
+            sd = sd(likes)) #calculate the mean number of likes and standard deviation
+
+
+###############################################################################
+# Conducting a paired samples t-test
+
+social_media$
+
+t.test(x=social_media$, 
+       y=social_media$,)
+
+
+###############################################################################
+# Bonus activity
+social_media %>% 
+  mutate(likes_diff = good_mood_likes - bad_mood_likes) #create a new column which is the difference
+# between good mood likes and bad mood likes
+
+# now amend the code above so that it starts with social_media_diff <- and run the code to create the new dataframe
+
+# now run the paired t.test
+t.test(x=social_media_diff$likes_diff)
