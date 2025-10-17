5fc7e083ac55c7bac5da485e27b7ed5598f2c31b
kel-github <getkellygarner@gmail.com>
2025-10-17 16:52:02 +1100

    updated script number 2

--- PSYC2001_ComputingTutorials/1_Introduction-to-R/your-first-script.R
+++ PSYC2001_ComputingTutorials/1_Introduction-to-R/your-first-script.R
@@ -9,7 +9,7 @@
 
 # Activity - Getting to know scripts
 # Type 2 + 2 below and run it so you can see the output in the console
-
+2 + 2
 
 # run the below line of code to see what answer you get
 mean(x=c(5, 4, 3))
@@ -19,16 +19,9 @@ mean(x=c(5, 4, 3))
 # copy and paste the code to make some objects here 
 
 
-# see Datatypes
+# see Activity: Explore Datatypes
 # run these lines of code to check the answers match
 # the book chapter
 typeof(age)
 typeof(new_year)
 typeof(name)
-
-# Looking after the environment
-# Complete the following line of code to remove data from the 
-# Environment. Note, delete this line of code before completing the 
-# final activity!
-rm()
-
--- PSYC2001_ComputingTutorials/2_Data-wrangling-and-visualisation/data_wrangling_and_visualisation.R
+++ PSYC2001_ComputingTutorials/2_Data-wrangling-and-visualisation/data_wrangling_and_visualisation.R
@@ -2,26 +2,31 @@
 # Data wrangling and visualisation lab.
 # This script will get you started on handling data and creating visualizations
 # in R and RStudio.
-# To complete the exercises in this script, follow Chapter X from
-# insert book here
+# To complete the exercises in this script, follow Chapter 2 Data wranglilng and 
+# visualisation - 
+# https://garner-code.github.io/PSYC2001_ComputingLabBook/data-wrangling-and-visualisation.html
 # K. Garner, 2025
 ###############################################################################
 
+# Before you do any activities!
 rm(list=ls()) # it is good practice to clear your environment at the start of 
 # your script
 
 ###############################################################################
-## Step 1: Copy and paste the code to install packages and load packages 
+## Activity - Load your packages
+## Copy and paste the code to install packages and load packages 
 
 
 ###############################################################################
-## Step 2: Find out more about the 'here' package.
+## Activity - Find out about packages
 ?here
 
+## Activity - Import the data
 ## Now finish and run the code that will import the social media data for 
 # you and store it to an object called social_media
 social_media <- 
 
+## Activity - View the data
 ## Now look at the data programmatically. Complete the following methods
 # to view the data. Also update the comments with missing information.
 
@@ -35,22 +40,25 @@ head() #head displays the first ...
 str()
 
 ###############################################################################
-## Summarise you data using the summary() function. Complete the code
+## Activity - Summarise the data
+# Summarise you data using the summary() function. Complete the code
 # and run it. What do you notice about the data?
 
 summary()
 
 
+## Activity - find and replace -999 values
 ## Complete the below code to find out how many cases of -999 there are in the 
 # time_on_social variable
 social_media_filtered <- filter(social_media, time_on_social == ) #keep all rows where `time_on_social` is equal to -999
 View() #view the filtered dataframe
 count(social_media_filtered) #count the number of rows in the filtered dataframe
 
-## Now do the same as above but using the pipe operator %>%
+## Activity - pipe with pipes
+## follow the piping activities to get the same results as you see in the chapter
 
 
-###############################################################################
+
 ## Time to replace those -999s with NAs, using mutate() and na_if().
 
 
@@ -59,24 +67,27 @@ count(social_media_filtered) #count the number of rows in the filtered dataframe
 
 
 ###############################################################################
+## Activity - save the cleaned data
 ## Save the data as a csv file using the write_csv() and here() functions.
 write.csv(social_media_NA, here("Output","PSYC2001_social-media-data-cleaned.csv"))
 
 ###############################################################################
-## Data visualisation
-# testing what we mean by making a canvas with ggplot
+## Activity - Make a blank canvas
+# testing what happens when we run ggplot() by itself. Complete the line of 
+# code below and run it
 social_media_NA %>%
   ggplot(aes(y = ))
 
-# Now add a boxplot to the canvas
+# Activity - add a boxplot / tidy up the x-axis /add some labels
 social_media_NA %>%
   ggplot(aes(y = )) + #ggplot uses aesthetic (aes()) to map axes. 
     geom_boxplot() 
   
-# Complete the code below to draw a histogram of the time_on_social variable
+# Activity - creating a histogram using ggplot()
 social_media_NA %>%
   ggplot(aes(x = )) + #ggplot uses aesthetic (aes()) to map axes. 
   geom_histogram() + #creates a histogram
   labs(x = "Time on social media (hours)", y = "Density") + #short for "labels", use to label axes and titles.
   theme_classic() #changes the theme of the plot to a classic theme. makes it prettier!
 # You can also use theme_minimal(), theme_light(), theme_dark() etc.
+
