d9e3854651c06e710e464bf2ebbe0e7589c564ef
kel-github <getkellygarner@gmail.com>
2025-10-17 13:23:47 +1100

    first script has references to the appropriate activities

--- PSYC2001_ComputingTutorials/1_Introduction-to-R/your-first-script.R
+++ PSYC2001_ComputingTutorials/1_Introduction-to-R/your-first-script.R
@@ -1,26 +1,32 @@
 ###############################################################################
 # Welcome to your first script.
 # This script will help you get started with R programming.
-# To complete the exercises in this script, follow Chapter X from
-# insert book here
+# To complete the exercises in this script, follow Chapter 01 
+# Introduction to R from 
+# https://garner-code.github.io/PSYC2001_ComputingLabBook/introduction-to-r.html
 # K. Garner, 2025
 ###############################################################################
 
+# Activity - Getting to know scripts
 # Type 2 + 2 below and run it so you can see the output in the console
 
 
 # run the below line of code to see what answer you get
 mean(x=c(5, 4, 3))
 
-# copy and paste the code to make some objects here 
-# see Activity: Create some objects
 
-# Datatypes: run these lines of code to check the answers match
+# see Activity: Create some objects
+# copy and paste the code to make some objects here 
+
+
+# see Datatypes
+# run these lines of code to check the answers match
 # the book chapter
 typeof(age)
 typeof(new_year)
 typeof(name)
 
+# Looking after the environment
 # Complete the following line of code to remove data from the 
 # Environment. Note, delete this line of code before completing the 
 # final activity!
