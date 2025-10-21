f0e952281765893db29002b29e82bc51ac786384
kel-github <getkellygarner@gmail.com>
2025-10-22 10:08:00 +1100

    updated script from lab 3 so that it matched the activities in the chapter

--- PSYC2001_ComputingTutorials/3_Testing-our-first-hypothesis/testing-first-hypothesis.R
+++ PSYC2001_ComputingTutorials/3_Testing-our-first-hypothesis/testing-first-hypothesis.R
@@ -9,13 +9,17 @@ rm(list=ls()) # it is good practice to clear your environment at the start of
 # your script
 
 ###############################################################################
-## Step 1: Copy and paste the code to install packages and load packages using
-# the library() function. Make sure you run the code by highlighting and running
-# it.
+# Check if packages are installed, if not install.
+if(!require(here)) install.packages('here') #checks if a package is installed and installs it if required.
+if(!require(tidyverse)) install.packages('tidyverse')
+
+## Activity - load packages using
+# the library() function. 
+
 
 
 ###############################################################################
-## Loading and checking data
+## Activity - Loading in the data
 
 # Now Amend the code below to read in your fresh data
 social_media <- read.csv(file = here(???,"PSYC2001_social-media-data-cleaned.csv"))
@@ -25,7 +29,7 @@ social_media <- read.csv(file = here(???,"PSYC2001_social-media-data-cleaned.csv
 
 
 ###############################################################################
-## Get the data into longform format
+## Activity - Get back on the pipes
 
 ## run this code so you can see what it does
 social_media %>% 
@@ -33,7 +37,7 @@ social_media %>%
 
 ## now run this code to pivot the data into longform format and see the results
 social_media %>% 
-  select("id","good_mood_likes","bad_mood_likes") %>% # choose which columns we want keep in our dataframe
+  select("id","good_mood_likes","bad_mood_likes") %>% # choose columns we want keep in our dataframe
   pivot_longer(cols = ends_with("likes"), names_to = "mood", values_to = "likes")
 
 ## now, very important! amend the code above so that the new dataframe is saved to an object called
@@ -44,16 +48,17 @@ social_media %>%
 
 
 ###############################################################################
+## Activity - start plotting
 ## Plotting the data real pretty
 social_media_likes %>% 
-  ggplot(aes(x = likes, group = mood, fill = mood)) + # set the aesthetics that will be worked 
-  # with on the canvas
+  ggplot(aes(x = likes, group = mood, fill = mood)) + # set canvas aesthetics
   geom_density() # use the data to draw a density plot 
 
 # bonus activity - can you use the export button in the plots pane to save the figure to 
 # the Output folder?
 
 ###############################################################################
+## Activity - get descriptive!
 ## Get descriptive statistics
 
 social_media_likes %>% 
@@ -63,6 +68,7 @@ social_media_likes %>%
 
 
 ###############################################################################
+## Activity - conducting a paired t-test
 # Conducting a paired samples t-test
 
 social_media$
